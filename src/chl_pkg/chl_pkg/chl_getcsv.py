a78dc2dbe85defa60c36bd912c5d93150c6e5114
Chabely Pecina <A01412387@tec.mx>
2025-09-30 11:56:43 -0400

    update1 chl_get.csv etc.

--- src/chl_pkg/chl_pkg/chl_getcsv.py
+++ src/chl_pkg/chl_pkg/chl_getcsv.py
@@ -1,40 +1,58 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import NavSatFix
 from std_msgs.msg import Float64
 import csv
 import os
-import time
+#from builtin_interfaces.msg import Time
 
 class ChlCSV(Node):
     def __init__(self):
         super().__init__('chl_csv')
-        
-        self.create_subscription(Float64, 'chl_data', self.chl_callback, 10)
 
-        # Archivo CSV
+        # Subs
+        self.create_subscription(NavSatFix, 'fix', self.gps_callback, 10)
+        self.create_subscription(Float64, 'chl_data', self.chl_callback, 10)
+        self.last_gps = None  
+
         self.filepath = os.path.join(os.getcwd(), "chl_data_log.csv")
         file_exists = os.path.isfile(self.filepath)
 
         self.csv_file = open(self.filepath, 'a', newline='')
         self.writer = csv.writer(self.csv_file)
 
-        # Encabezado si el archivo es nuevo
+        
         if not file_exists:
-            self.writer.writerow(["Time (s)", "Chl μg/l"])
+            self.writer.writerow(["time", "latitude", "longitude", "chl_value"])
+
+        self.get_logger().info(f"Guardando datos en {self.filepath}")
+
+    def gps_callback(self, msg: NavSatFix):
+        #keep last msg from gps
+        self.last_gps = msg
 
     def chl_callback(self, msg: Float64):
-        # Tiempo del sistema (epoch en segundos con decimales)
-        time_sec = time.time()
+        if self.last_gps is None:
+            self.get_logger().warn("No hay datos de GPS todavía, ignorando chl_data")
+            return
+    
+        stamp: Time = self.last_gps.header.stamp
+        time_sec = stamp.sec + stamp.nanosec * 1e-9
 
-        row = [time_sec, msg.data]
+        row = [
+            time_sec,
+            self.last_gps.latitude,
+            self.last_gps.longitude,
+            msg.data
+        ]
 
         self.writer.writerow(row)
-        self.csv_file.flush()  # escritura inmediata
-        self.get_logger().info(f"Saved row: {row}")
+        self.csv_file.flush()  
+        self.get_logger().info(f"Fila guardada: {row}")
 
     def destroy_node(self):
-        # Cerrar archivo al destruir el nodo
+        
         self.csv_file.close()
         super().destroy_node()
 
